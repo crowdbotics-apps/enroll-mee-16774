fde3649f60d317e7382b50be64b59edb0d3761f7
Crowdbotics <team@crowdbotics.com>
2020-05-08 23:32:59 +0000

    Update models

--- backend/enroll_mee_16774/wsgi.py
+++ backend/enroll_mee_16774/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'enroll_mee_16774.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "enroll_mee_16774.settings")
 
 application = get_wsgi_application()
